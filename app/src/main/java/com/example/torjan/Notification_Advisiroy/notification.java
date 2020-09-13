c461cce754a05be295c49949a387f79c08038bc8
mabhinav004 <mabhinav004@gmail.com>
2020-09-13 09:13:27 +0530

    Tab Layout..

--- app/src/main/java/com/example/torjan/Notification_Advisiroy/notification.java
+++ app/src/main/java/com/example/torjan/Notification_Advisiroy/notification.java
@@ -3,10 +3,8 @@ package com.example.torjan.Notification_Advisiroy;
 import com.google.gson.annotations.SerializedName;
 
 public class notification {
-    @SerializedName("title")
     private String title;
 
-    @SerializedName("link")
     private String link;
 
     public String getTitle() {
